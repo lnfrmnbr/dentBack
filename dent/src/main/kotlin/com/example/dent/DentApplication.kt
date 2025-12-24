ddef4a1ccf4d91e76b40ce8b93aa140c9c987c64
Домбровская Виктория <v.dombrovskaya@bftcom.com>
2025-12-24 16:37:11 +0300

    Initial commit

---
+++ dent/src/main/kotlin/com/example/dent/DentApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dent
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DentApplication
+
+fun main(args: Array<String>) {
+	runApplication<DentApplication>(*args)
+}
